73d19b82d99796a1c46d318fc2bafeaedb7bb8e6
Ernestekas <ernestekas@gmail.com>
2022-01-06 11:03:42 +0200

    Fully working app. Commiting before cleaning code.

---
+++ MVC_P0105_Currency_Exchange_Rate/MVC_P0105_Currency_Exchange_Rate/wwwroot/js/daysCalculator.js
@@ -0,0 +1,20 @@
+﻿function changed() {
+    let yearDD = document.getElementById('year').value;
+    let monthDD = document.getElementById("month").value;
+    let dayDD = document.getElementById("day").value;
+
+    let maxDaysPerMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    if (yearDD % 4 === 0) {
+        maxDaysPerMonth[1] = 29;
+    }
+
+    let day = document.getElementById("day");
+    for (let i = day.options.length - 1; i >= 0; i--) {
+        day.remove(i);
+    }
+
+    for (let i = 0; i < maxDaysPerMonth[monthDD - 1]; i++) {
+        day.options[i] = new Option(i + 1, i + 1);
+    }
+}
